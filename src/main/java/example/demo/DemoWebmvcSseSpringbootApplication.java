52bbb7eb3fb733c9186588f06af4ebb91f4d7095
merge-simpson <letsdev@kakao.com>
2024-01-22 04:36:46 +0900

    fix(cors): let main class scan cors package

--- src/main/java/example/demo/DemoWebmvcSseSpringbootApplication.java
+++ src/main/java/example/demo/DemoWebmvcSseSpringbootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "example")
 @ConfigurationPropertiesScan(basePackages = "example")
 public class DemoWebmvcSseSpringbootApplication {
 
