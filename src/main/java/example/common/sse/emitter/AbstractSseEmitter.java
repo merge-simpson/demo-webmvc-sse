e5caf814386ad4ba6ee4b7329ead6a17367b8828
merge-simpson <letsdev@kakao.com>
2024-01-23 09:08:17 +0900

    chore(sse): AbstractSseEmitter.java 생성자 문서화 주석 설명 보충, 불필요 임포트 제거 등

--- src/main/java/example/common/sse/emitter/AbstractSseEmitter.java
+++ src/main/java/example/common/sse/emitter/AbstractSseEmitter.java
@@ -1,6 +1,5 @@
 package example.common.sse.emitter;
 
-import lombok.experimental.SuperBuilder;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.time.Instant;
@@ -12,12 +11,13 @@ public abstract class AbstractSseEmitter<KEY>
     protected Instant createdAt;
 
     /**
-     *
-     * @param id
+     * 관리 호환성을 위해 만든 AbstractSseEmitter 클래스 생성자
+     * @param id 여러 관리 방식의 호환성을 위해 아이디를 필수에 준하게 관리할 필요가 있음.
      * @param timeout milliseconds
      */
     protected AbstractSseEmitter(KEY id, Long timeout) {
         super(timeout);
+
         this.id = id;
         createdAt = Instant.now();
     }
